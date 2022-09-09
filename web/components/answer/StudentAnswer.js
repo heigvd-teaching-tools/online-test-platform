98660283af3a0211a1a1626156edae6ab0a8c22d
Ovich <stefanteofanovic@hotmail.com>
2022-09-10 00:57:39 +0200

    Take Exam Layout V2 -> next / back on bottom, spacing

--- web/components/answer/StudentAnswer.js
+++ web/components/answer/StudentAnswer.js
@@ -29,10 +29,10 @@ const StudentAnswer = ({ question, page, totalPages, onAnswer }) => {
         <>
         <ResizePanel 
             leftPanel={
-                <Stack spacing={2} sx={{ overflow:'auto', minWidth: 0, height:'100%', pl:2, pt:2, pr:1 }}>
+                <Stack spacing={2} sx={{ overflow:'auto', minWidth: 0, height:'100%', pl:2, pt:2, pr:1, pb:1 }}>
                     <Stack direction="row" alignItems="center" spacing={1}>
                         <Column width="32px"><Image alt="Loading..." src={`/svg/questions/${question.type}.svg`} layout="responsive" width="32px" height="32px" priority="1" /></Column>
-                        <Column right><Typography variant="body1">{displayQuestionType(question.type)}</Typography></Column>
+                        <Column right><Typography variant="body1"><b>Q{page}</b> / {totalPages} </Typography></Column>
                         <Column flexGrow={1} right><Chip color="info" label={`${question.points} pts`} /></Column>
                     </Stack>
                     <Stack flexGrow={1}>
@@ -43,7 +43,7 @@ const StudentAnswer = ({ question, page, totalPages, onAnswer }) => {
                     </Stack>            
                     <Stack direction="row" justifyContent="space-between" alignItems="center">
                             <Button color="primary" disabled={page === 1} onClick={previousPage}>Previous</Button>
-                            <Typography variant="body1">{page} / <b>{totalPages}</b></Typography>
+                            <Typography variant="body1"><b>{page} / {totalPages}</b></Typography>
                             <Button color="primary" disabled={page === totalPages} onClick={nextPage}>Next</Button>
                     </Stack>
                 </Stack>
@@ -58,7 +58,7 @@ const StudentAnswer = ({ question, page, totalPages, onAnswer }) => {
             }
         />
         { question.type === 'code' && (
-            <Paper square sx={{ p:1, mt:1 }}>
+            <Paper elevation={2} square sx={{ p:1, mt:1 }}>
                 <CodeTestResult 
                     code={question.studentAnswer ? question.studentAnswer.code : question.code}
                     questionId={question.id}
@@ -68,21 +68,6 @@ const StudentAnswer = ({ question, page, totalPages, onAnswer }) => {
         
         </>
     )
-}
-
-const displayQuestionType = (type) => {
-    switch(type){
-        case 'multipleChoice':
-            return "Multiple Choice";
-        case 'essay':
-            return "Essay";
-        case 'trueFalse':
-            return "True/False";
-        case 'code':
-            return "Code Challenge";
-        default:
-            return "N/A";
-    }
 }
 
 export default StudentAnswer;
