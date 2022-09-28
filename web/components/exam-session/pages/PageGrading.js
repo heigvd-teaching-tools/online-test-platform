b879bf9508dfca44fdffaef6830dfb27da96dc88
Ovich <stefanteofanovic@hotmail.com>
2022-09-28 11:46:17 +0200

    Extract PiePercent component - success rate, grading progress

--- web/components/exam-session/pages/PageGrading.js
+++ web/components/exam-session/pages/PageGrading.js
@@ -173,8 +173,6 @@ const PageGrading = () => {
                                 setFilter(filter);
                             }}
                         />
-                        
-                        
                     </Stack>
                 }
                 leftPanel={
