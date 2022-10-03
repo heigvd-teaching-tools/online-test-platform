7edd15e62f494a753b3c4243cf80868af1aaf9e3
Ovich <stefanteofanovic@hotmail.com>
2022-10-03 11:48:32 +0200

    Remove scheduling page -> prod build error fix

--- web/pages/exam-sessions/[sessionId]/scheduling.js
+++
@@ -1,9 +0,0 @@
-import { ExamSessionProvider } from '../../../context/ExamSessionContext';
-import PageScheduling from '../../../components/exam-session/pages/PageScheduling';
-
-const Scheduling = () => 
-<ExamSessionProvider>
-    <PageScheduling />
-</ExamSessionProvider>
-
-export default Scheduling;
