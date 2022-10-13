aa23da431340d03cb1cf511f2f9edd0623527468
Ovich <stefanteofanovic@hotmail.com>
2022-10-13 18:02:08 +0200

    SandBox -> Only authenticated users

--- web/pages/api/code/index.js
+++ web/pages/api/code/index.js
@@ -1,6 +1,16 @@
 import { runSandbox } from "../../../sandbox/runSandbox";
+import { hasRole } from '../../../utils/auth';
+import {Role} from "@prisma/client";
 
 export default async function handler(req, res) {
+    // Student Code test runs concerns the student answer
+    let isStudent = await hasRole(req, Role.STUDENT);
+    let isProf = await hasRole(req, Role.PROFESSOR);
+
+    if(!(isStudent || isProf)){
+        res.status(401).json({ message: 'Unauthorized' });
+        return;
+    }
 
     let { code } = req.body;
     await runSandbox(code).then((reponse) => {
