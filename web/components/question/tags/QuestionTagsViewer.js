639f6012dbf005bdfacc393b4b9b4586afadf2a4
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2023-04-27 10:21:43 +0200

    fixing tags placement

--- web/components/question/tags/QuestionTagsViewer.js
+++ web/components/question/tags/QuestionTagsViewer.js
@@ -1,11 +1,12 @@
-import {Chip, Stack, Typography} from "@mui/material";
+import {Box, Chip, Stack, Typography} from "@mui/material";
 
 const QuestionTagsViewer = ({ tags = [], size = "medium" } ) =>
-    <Stack direction={"row"} spacing={1}>{
+    <Stack direction={"row"} rowGap={1} flexWrap="wrap">
+        {
         tags?.map((tag) => (
             <Chip size={size} key={tag.label} color={"info"} variant="filled" label={
                 <Typography variant={"caption"}>{tag.label}</Typography>
-            } />
+            } sx={{ mr : 1}}  />
         ))}
     </Stack>
 
