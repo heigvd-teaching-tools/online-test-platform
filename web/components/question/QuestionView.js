d4818829915418285e0f794afd1873a4aa1cd364
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2023-05-17 16:02:25 +0200

    fixing minor issues

--- web/components/question/QuestionView.js
+++ web/components/question/QuestionView.js
@@ -7,7 +7,7 @@ import ScrollContainer from "../layout/ScrollContainer";
 const QuestionView = ({ order, points, question, totalPages }) => {
 
     return (
-        <Stack height={"100%"} spacing={2} overflow={"auto"} pl={2} pt={2} pr={1} pb={1} border={"1px dashed red"}>
+        <Stack height={"100%"} width={"100%"} spacing={2} overflow={"auto"} pl={2} pt={2} pr={1} pb={1}>
             <Stack direction="row" alignItems="center" spacing={1}>
                 <Column width="32px"><Image alt="Loading..." src={`/svg/questions/${question.type}.svg`} layout="responsive" width="32px" height="32px" priority="1" /></Column>
                 <Column right><Typography variant="body1"><b>Q{order + 1}</b> / {totalPages} </Typography></Column>
