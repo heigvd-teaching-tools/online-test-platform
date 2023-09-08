698e26c2b8b287dc47f97f5630e0c572303d68f7
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2023-09-08 14:08:13 +0200

    query output side by side dynamic height

--- web/components/layout/LayoutSplitScreen.js
+++ web/components/layout/LayoutSplitScreen.js
@@ -2,27 +2,23 @@ import { Paper, Stack } from '@mui/material'
 import ResizePanel from './utils/ResizePanel'
 import ScrollContainer from './ScrollContainer'
 const LayoutSplitScreen = ({
-  subheader,
-  leftPanel,
-  rightPanel,
-  footer,
-  rightWidth = 60,
+    subheader,
+    leftPanel,
+    rightPanel,
+    footer,
+    rightWidth = 60,
     height = "100%",
     useScrollContainer = true
 }) => {
   return (
-    <Stack height={height} maxHeight={height}>
+    <Stack height={height} maxHeight={height} minHeight={height}>
       {subheader && subheader}
       <Stack flex={1} alignItems="center" maxHeight="100%">
         <ResizePanel
           rightWidth={rightWidth}
           leftPanel={<ScrollContainer>{leftPanel}</ScrollContainer>}
           rightPanel={
-            <Paper
-              square
-              elevation={0}
-              sx={{ height: '100%', overflow: 'hidden' }}
-            >
+            <Paper square elevation={0} sx={{ height: '100%', overflow: 'hidden' }}>
                 {useScrollContainer ? (
                   <ScrollContainer spacing={0} padding={0}>
                     {rightPanel}
