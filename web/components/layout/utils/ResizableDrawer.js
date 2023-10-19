1c8c8de7dbe001cf15bc53f02ffb628461f94731
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2023-10-19 13:24:03 +0200

    fixing mui warnings

--- web/components/layout/utils/ResizableDrawer.js
+++ web/components/layout/utils/ResizableDrawer.js
@@ -16,7 +16,7 @@ const ResizableDrawer = ({ open, width:initial=70, onClose, children }) => {
             <Box pl={1} width={"100%"} height={"100%"} position={"relative"}>
                 
                 { dragging && (
-                    <Stack position={"fixed"} top={0} left={0} right={0} bottom={0} width={"100vw"} height={"100vh"} zIndex={100} border={"1px dashed red"} bgcolor={"rgba(255,255,255,0.5)"}
+                    <Stack position={"fixed"} top={0} left={0} right={0} bottom={0} width={"100vw"} height={"100vh"} zIndex={100}
                         onMouseMove={(e) => {
                             setWidth((window.innerWidth - e.clientX) / window.innerWidth * 100);
                         }}
