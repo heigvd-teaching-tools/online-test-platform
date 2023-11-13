0fa8c68aa1ebad4ff3d3326c0999d63927490241
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2023-11-13 19:42:29 +0100

    use absolute import path

--- web/pages/api/[groupScope]/jam-sessions/[jamSessionId]/consult/[userEmail].js
+++ web/pages/api/[groupScope]/jam-sessions/[jamSessionId]/consult/[userEmail].js
@@ -1,10 +1,7 @@
 import { Role } from '@prisma/client'
-import { withAuthorization, withMethodHandler } from '../../../../../../middleware/withAuthorization'
-import { withPrisma } from '../../../../../../middleware/withPrisma'
-import {
-  IncludeStrategy,
-  questionIncludeClause,
-} from '../../../../../../code/questions'
+import { withAuthorization, withMethodHandler } from '@/middleware/withAuthorization'
+import { withPrisma } from '@/middleware/withPrisma'
+import { IncludeStrategy, questionIncludeClause } from '@/code/questions'
 /*
   Professor can consult the users's answers to the questions of a jam session
 */
@@ -37,6 +34,7 @@ const get = async (req, res, prisma) => {
             },
         },
     });
+    console.log("get called")
     res.status(200).json(jamSession)
 
 }
