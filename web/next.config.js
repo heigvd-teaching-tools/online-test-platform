cd61e92b424f51786d3699586d55593729a0f3d5
Perdjesk <Perdjesk@users.noreply.github.com>
2024-03-11 13:37:18 +0100

    state

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   swcMinify: true
 }
 
-module.exports = nextConfig
