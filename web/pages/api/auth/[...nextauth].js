430669d65859db3042d6deb6b423b4c0a67f03a9
Teofanovic Stefan <stefan.teofanovic@hes-so.ch>
2025-01-30 11:32:55 +0100

    single auth

--- web/pages/api/auth/[...nextauth].js
+++ web/pages/api/auth/[...nextauth].js
@@ -43,6 +43,18 @@ const MyAdapter = {
   },
 }
 
+const keycloakProvider = KeycloakProvider({
+  clientId: process.env.NEXTAUTH_KEYCLOAK_CLIENT_ID,
+  clientSecret: process.env.NEXTAUTH_KEYCLOAK_CLIENT_SECRET,
+  issuer: process.env.NEXTAUTH_KEYCLOAK_ISSUER_BASE_URL,
+  authorization: {
+    params: {
+      redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/keycloak`,
+    },
+  },
+})
+
+
 const switchLegacyProvider = {
   id: 'switch_legacy',
   name: 'SWITCH edu-ID (legacy)',
@@ -74,12 +86,6 @@ const switchLegacyProvider = {
     /*
     Called on each successful login with an OAuth provider
     */
-
-    const organizationDomain = process.env.NEXTAUTH_SWITCH_ORGANIZATION_DOMAIN
-
-    if (!organizationDomain) {
-      return null
-    }
 
     return {
       id: OAuthProfile.sub,
@@ -127,25 +133,24 @@ const switchEduId = {
     text: '#000', // Black text color
   },
   profile(OAuthProfile) {
-    /*
-    Called on each successful login with an OAuth provider
-    */
+    const allowedDomains =
+      process.env.NEXTAUTH_SWITCH_ORGANIZATION_DOMAINS?.split(',').map(
+        (domain) => domain.trim(),
+      )
 
-    const organizationDomain = process.env.NEXTAUTH_SWITCH_ORGANIZATION_DOMAIN
-
-    if (!organizationDomain) {
-      throw new Error('Organization domain is not set.')
+    if (!allowedDomains || allowedDomains.length === 0) {
+      throw new Error('Allowed organization domains are not set.')
     }
 
     const affiliations = OAuthProfile.swissEduIDLinkedAffiliationMail || []
 
     const email = affiliations.find((affiliation) =>
-      affiliation.endsWith(organizationDomain),
+      allowedDomains.some((domain) => affiliation.endsWith(domain)),
     )
 
     if (!email) {
       throw new Error(
-        `User does not have an appropriate affiliation for ${organizationDomain}`,
+        `User does not have an appropriate affiliation for the allowed domains: ${allowedDomains.join(', ')}`,
       )
     }
 
@@ -164,20 +169,10 @@ const switchEduId = {
   },
 }
 
-const keycloakProvider = KeycloakProvider({
-  clientId: process.env.NEXTAUTH_KEYCLOAK_CLIENT_ID,
-  clientSecret: process.env.NEXTAUTH_KEYCLOAK_CLIENT_SECRET,
-  issuer: process.env.NEXTAUTH_KEYCLOAK_ISSUER_BASE_URL,
-  authorization: {
-    params: {
-      redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/keycloak`,
-    },
-  },
-})
 
 export const authOptions = {
   adapter: MyAdapter,
-  providers: [switchLegacyProvider, switchEduId, keycloakProvider],
+  providers: [switchEduId],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, user }) {
